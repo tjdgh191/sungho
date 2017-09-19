14a1a7fa21732f8c1a1e4b70b87a64f6fedc5dad
3e <3e@DESKTOP-HNAAQ41>
2017-09-19 22:32:31 +0900

    main 추가

--- Sungho/src/PhoneInfo.java
+++ Sungho/src/PhoneInfo.java
@@ -33,19 +33,5 @@ public class PhoneInfo {
 		else System.out.println("생일 : " + birthday);
 	}
 	
-	public static void main(String args[])
-	{
-		Scanner fa = new Scanner(System.in);
-		System.out.println("이름을 입력해주세요");
-		String nam = fa.nextLine();
-		System.out.println("전화번호를 입력해주세요");
-		String phone = fa.nextLine();
-		System.out.println("생일을 입력해주세요 // 입력안해도 됨");
-		String birth = fa.nextLine();
-		
-		PhoneInfo fr = new PhoneInfo(nam,phone,birth);
-		
-		fr.printInfo();
-		fa.close();
-	}
+	
 }
